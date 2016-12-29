0022abd6f8f987e26dac33dc202441c290a8ba7e
Corneliuscob <NGai_to_lo@hotmail.com>
2016-12-29 13:33:55 -0800

    tons

--- server.js
+++ server.js
@@ -1,15 +1,4 @@
 
-// include the http module
-var http = require('http');
-
-// create a webserver
-http.createServer(function (req, res) {
-
-    // respond to any incoming http request
-    res.writeHead(200, {'Content-Type': 'text/plain'});
-    res.end('Hello World\n');
-
-}).listen(3000, '127.0.0.1');
 // include the express module
 var express = require('express');
 //cant get this line to work
