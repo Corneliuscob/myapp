4878e6d41ea0c8f2c16c156d03d1e7c05e30eb0f
Corneliuscob <NGai_to_lo@hotmail.com>
2016-12-29 13:38:23 -0800

    tons

--- server.js
+++ server.js
@@ -1,13 +1,18 @@
-
-// include the express module
 var express = require('express');
-//cant get this line to work
-// var app = express.createServer();
-var app =express();
-app.get('/', function(req, res){
-    res.send('Hello World');
+var app = express();
+
+app.set('port', (process.env.PORT || 5000));
+
+app.use(express.static(__dirname + '/public'));
+
+// views is directory for all template files
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
+
+app.get('/', function(request, response) {
+  response.render('pages/index');
 });
-app.listen(5000,listening);
-function listening(){
-	console.log("listening");
-}
+
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
+});
